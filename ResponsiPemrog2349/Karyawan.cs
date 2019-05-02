7f975f85285513a325da2ef3f2574cb16c09acb7
exksa <dylanexksa@gmail.com>
2019-05-03 00:24:27 +0700

    Beta

---
+++ ResponsiPemrog2349/Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2349
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int Bonus1 { get; set; }
+
+        public Karyawan()
+        {
+            Nik = "18112349";
+            Nama = "Dylan";
+            GajiBulanan = 3000000;
+        }
+
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            this.Nik = nik;
+            this.Nama = nama;
+            this.GajiBulanan = gajibulanan;
+        }
+
+        
+    }
+}
---
+++ ResponsiPemrog2349/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2349
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class mahasiswa
+            Karyawan me = new Karyawan();
+            Karyawan you = new Karyawan("18112365", "Dwi", 2000000);
+
+            Console.WriteLine("No.  Nik / Nama          Gaji Bulanan");
+            Console.Write("1.   {0}", me.Nik.ToString());
+            Console.Write(" / {0}", me.Nama.ToString());
+            Console.WriteLine("    {0}", me.GajiBulanan.ToString());
+            Console.Write("2.   {0}", you.Nik.ToString());
+            Console.Write(" / {0}", you.Nama.ToString());
+            Console.WriteLine("      {0}", you.GajiBulanan.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("Asyik kenaikan gaji 10%");
+            Console.WriteLine();
+
+            var hasil = (me.GajiBulanan * 0.10) + me.GajiBulanan;
+            var hasil2 = (you.GajiBulanan * 0.10) + you.GajiBulanan;
+
+            Console.WriteLine("No.  Nik / Nama          Gaji Bulanan");
+            Console.Write("1.   {0}", me.Nik.ToString());
+            Console.Write(" / {0}", me.Nama.ToString());
+            Console.WriteLine("    {0}", hasil);
+            Console.Write("2.   {0}", you.Nik.ToString());
+            Console.Write(" / {0}", you.Nama.ToString());
+            Console.WriteLine("      {0}", hasil2);
+
+            Console.ReadKey();
+        }
+
+        static int Bonus(int gajibulanan)
+        {
+            return gajibulanan * (10 / 100);
+        }
+    }
+}
